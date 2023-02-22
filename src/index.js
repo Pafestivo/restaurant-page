28bb88964be28742319785f109bcd6691304dc92
Or Rodrigez <orrodrigez1@gmail.com>
2023-02-22 15:18:55 +0200

    Add effects to the navigation menu

--- src/index.js
+++ src/index.js
@@ -14,3 +14,14 @@ const contactNav = document.getElementById('contact');
 homeNav.addEventListener('click', loadHomePage);
 menuNav.addEventListener('click', loadMenu);
 contactNav.addEventListener('click', loadContact);
+homeNav.addEventListener('click', highlight);
+menuNav.addEventListener('click', highlight);
+contactNav.addEventListener('click', highlight);
+
+function highlight() {
+  homeNav.classList.remove('highlight');
+  menuNav.classList.remove('highlight');
+  contactNav.classList.remove('highlight');
+
+  this.classList.add('highlight');
+}
